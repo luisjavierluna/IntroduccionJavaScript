9eb9c7f32926f04f3d9d1e9b52457979ff5186ab
luisjavierluna <javierlunaluis@gmail.com>
2022-11-20 15:23:10 -0600

    104. Variables en JavaScript

---
+++ js/01.js
@@ -0,0 +1,24 @@
+// Variables
+
+var product= 'Game Headphones' // Inicializar variable y asignarle valor
+
+var available // Inicializar variable, pero sin valor
+
+product = true; // Reasignando el valor a una variable
+
+available = true // Asignando valor a una variable previamente inicializada, pero que no se había asignado
+
+var product1 = 'Computer',
+    available1 = true,
+    category = 'Computers'; // Al aisgnar varias variables al mismo tiempo se si se cierran con ;
+
+// var 1availabel // No se puede iniciar nombres de variables con números
+var available_
+
+// Estilos para las variables
+var name_product = 'Monitor HD' // underscore
+var nameProduct = 'Monitor HD' // Camelcase
+var NameProduct = 'Monitor HD' // Pascalcase
+var nameroduct = 'Monitor HD'
+
+console.log(product)
