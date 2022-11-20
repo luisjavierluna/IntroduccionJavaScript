d6b5dc4b7dd658eaae3b830d63ba99388231b7f7
luisjavierluna <javierlunaluis@gmail.com>
2022-11-20 15:41:59 -0600

    105. Variables con let y const

---
+++ js/const.js
@@ -0,0 +1,23 @@
+// Constantes con const
+
+const product= 'Game Headphones' // Inicializar variable y asignarle valor
+
+// const available // con const no se puede inicializar constantes sin valor
+const available = true
+
+// product = true; // No se puede reasignar una constante
+
+// available = true // No se puede reasignar una constante
+
+const product1 = 'Computer',
+    available1 = true,
+    category = 'Computers'; // Al aisgnar varias constantes al mismo tiempo se si se cierran con ;
+
+// const 1availabel // No se puede iniciar nombres de constantes con números
+const available_ = true
+
+// Estilos para las constantes
+const name_product = 'Monitor HD' // underscore
+const nameProduct = 'Monitor HD' // Camelcase
+const NameProduct = 'Monitor HD' // Pascalcase
+const nameroduct = 'Monitor HD'
---
+++ js/let.js
@@ -0,0 +1,22 @@
+// Variables con let
+
+let product= 'Game Headphones' // Inicializar variable y asignarle valor
+
+let available // Inicializar variable, pero sin valor
+
+product = true; // Reasignando el valor a una variable
+
+available = true // Asignando valor a una variable previamente inicializada, pero que no se había asignado
+
+let product1 = 'Computer',
+    available1 = true,
+    category = 'Computers'; // Al aisgnar varias variables al mismo tiempo se si se cierran con ;
+
+// let 1availabel // No se puede iniciar nombres de variables con números
+let available_
+
+// Estilos para las variables
+let name_product = 'Monitor HD' // underscore
+let nameProduct = 'Monitor HD' // Camelcase
+let NameProduct = 'Monitor HD' // Pascalcase
+let nameroduct = 'Monitor HD'
