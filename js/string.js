72f0152924353d770e9f77b949dbbd0a27250f14
luisjavierluna <javierlunaluis@gmail.com>
2022-11-20 16:56:20 -0600

    106. Strings o Cadenas de Texto

---
+++ js/string.js
@@ -0,0 +1,9 @@
+// Strings o cadenas de texto
+
+const product = '20" monitor'
+// const product2 = String("30 inches monitor")
+// const product3 = new String ("50 inches monitor")
+
+console.log(product)
+// console.log(product2)
+// console.log(product3)
