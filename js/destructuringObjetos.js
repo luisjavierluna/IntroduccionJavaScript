75165fdcf627a87694487b32da493afc18a7649b
luisjavierluna <javierlunaluis@gmail.com>
2022-11-26 09:12:25 -0600

    115. Destructuring de Objetos

---
+++ js/destructuringObjetos.js
@@ -0,0 +1,14 @@
+// Objetos
+
+const producto = {
+    nombreProducto: "Monitor 20 pulgadas",
+    precio: 300,
+    disponible: true
+}
+
+// Destructuring
+
+const {precio, nombreProducto} = producto
+
+console.log(precio)
+console.log(nombreProducto)
