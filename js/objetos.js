13ce9f9c6ea27a6171f062ead5add6f2a2412125
luisjavierluna <javierlunaluis@gmail.com>
2022-11-26 08:53:31 -0600

    113. Objetos

---
+++ js/objetos.js
@@ -0,0 +1,19 @@
+// Objetos
+
+const nombreProducto = "Monitor 20 pulgadas"
+const precio = 300
+const disponible = true
+
+const producto = {
+    nombreProducto: "Monitor 20 pulgadas",
+    precio: 300,
+    disponible: true
+}
+
+console.log(producto)
+
+console.log(producto.nombreProducto)
+console.log(producto.precio)
+console.log(producto.disponible)
+
+console.log(producto["precio"]) // También debe funcionar como las alternativas de arriba
