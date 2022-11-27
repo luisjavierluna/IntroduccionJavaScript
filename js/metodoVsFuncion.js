c4928501460d2b6a369564a33106d552abe0f782
luisjavierluna <javierlunaluis@gmail.com>
2022-11-27 12:09:20 -0600

    123. Diferencia de un Método y una función

---
+++ js/metodoVsFuncion.js
@@ -0,0 +1,5 @@
+const numero1 = 20
+const numero2 = "20"
+
+console.log(parseInt(numero2)) // funcion, primero se pone el nombre y después dentro de parentesis la variable que cambiará
+console.log(numero1.toString()) // método, primero se pone la variable que cambiará y despues con un punto el método
