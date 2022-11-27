bb6d8a394a6b17a0b629a6e375450a95924c58e0
luisjavierluna <javierlunaluis@gmail.com>
2022-11-27 12:00:03 -0600

    121. Funciones en JavaScript

---
+++ js/funciones.js
@@ -0,0 +1,25 @@
+// Declaración de Función
+
+function sumar() {
+    console.log(10 + 10)
+}
+
+sumar()
+
+// Expresión de la función
+const sumar2 = function() {
+    console.log(3 + 3)
+}
+
+
+
+
+
+
+// IIFE (Sintaxis como la jQuery), hacen que no se puede usar este código en otro archivo
+
+// (function() {
+//     console.log('Esto es una función')
+// })()
+
+// console.log(cliente) // Cliente sería una variable de otro archivo dentro de una función IIFE, es decir, no funcinaria porque estaría protegida
