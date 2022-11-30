4a9f5219070146fc94bdb50c387c63ad9d66d111
luisjavierluna <javierlunaluis@gmail.com>
2022-11-30 12:06:47 -0600

    137. POO - Herencia

--- js/clases.js
+++ js/clases.js
@@ -9,10 +9,34 @@ class Producto {
     formatearProducto() {
         return `El Producto ${this.nombre} tiene un precio de: $ ${this.precio}`
     }
+
+    obtenerPrecio() {
+        console.log(this.precio)
+    }
 }
 
 const producto2 = new Producto('Monitor Curvo de 49"', 800)
 const producto3 = new Producto('Laptop', 300)
 
-console.log(producto2)
-console.log(producto3)
+// Herencia
+
+class Libro extends Producto {
+    constructor(nombre, precio, isbn) {
+        super(nombre, precio) // super se usa para heredar el constructor de la clase Producto
+        this.isbn = isbn
+    }
+
+    formatearProducto() {
+        return `${super.formatearProducto() } y su ISBN es ${this.isbn}`
+    }
+
+    obtenerPrecio() {
+        super.obtenerPrecio()
+        console.log('Y si hay en existencia')
+    }
+}
+
+const libro = new libro('JavaScript la Revolución', 120, '1039847089374')
+console.log(libro.formatearProducto)
+console.log(libro.obtenerPrecio)
+console.log(producto2.formatearProducto())
