c7e2194366a93c86cd620c148956636a26b9c946
luisjavierluna <javierlunaluis@gmail.com>
2022-11-30 10:17:58 -0600

    134. Object Constructor y Object Literal

---
+++ js/objLiteralYObjConstructor.js
@@ -0,0 +1,19 @@
+// POO
+
+// Object Literal
+const producto = {
+    nombre: 'Tablet',
+    precio: 500
+}
+
+// Object Constructor
+function Producto(nombre, precio) {
+    this.nombre = nombre
+    this.precio = precio
+}
+
+const producto2 = new Producto('Monitor Curvo de 49"', 800)
+const producto3 = new Producto('Laptop', 300)
+
+console.log(producto2)
+console.log(producto3)
