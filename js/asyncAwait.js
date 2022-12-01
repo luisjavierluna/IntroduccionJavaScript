1bac94f9b7574e9eeb3e7e683c40485f7c12d484
luisjavierluna <javierlunaluis@gmail.com>
2022-12-01 11:41:13 -0600

    141. Async / Await

---
+++ js/asyncAwait.js
@@ -0,0 +1,22 @@
+// Async / await
+
+function descargarNuevosClientes() {
+    return new Promise( resolve => {
+        console.log('Descargando clientes... espere...')
+
+        setTimeout(() => {
+            resolve('Los Clientes fueron descargados')
+        }, 5000);
+    })
+}
+
+async function app() {
+    try {
+        const resultado = await descargarNuevosClientes()
+        console.log(resultado)
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+app()
